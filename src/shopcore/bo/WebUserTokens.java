397a99bfd2517e39ff3f617ab28f950971989d01
magnus-1 <magnus-1@users.noreply.github.com>
2016-10-03 15:14:32 +0200

    javadocing for token

--- src/shopcore/bo/WebUserTokens.java
+++ src/shopcore/bo/WebUserTokens.java
@@ -11,6 +11,15 @@ public class WebUserTokens implements AuthUser {
     private int userId = 0;
     private String sessionId;
     private int securityLevel = 0;
+
+    /**
+     * Creates a webUserToken and generate a token that can be  used to identify this user
+     * @param userId the user id
+     * @param user user name
+     * @param pass password
+     * @param sessionId the session id for this token
+     * @param securityLevel what security level this token can have access to
+     */
     public WebUserTokens(int userId, String user, String pass, String sessionId,int securityLevel) {
         // TODO: encrypt token, and sign it
         this.userToken = ":" + user + ":" + pass + ":" + sessionId + ":" + userId + ":" +securityLevel;
@@ -20,6 +29,12 @@ public class WebUserTokens implements AuthUser {
         System.out.println("Token is:" + userToken);
     }
 
+    /**
+     * This creates authToken from a String representation of a token,
+     * it also checks that the token is vaild
+     * @param authToken the token
+     * @throws SecurityException if this token is not vailid in any way
+     */
     public WebUserTokens(String authToken) throws SecurityException {
         String delimiter = ":";
         // TODO: decrypt token, and cehck its sign
@@ -41,19 +56,36 @@ public class WebUserTokens implements AuthUser {
         this.userToken = authToken;
     }
 
+    /**
+     * Get the token in string form
+     * @return
+     */
     @Override
     public String getAuthToken() {
         return userToken;
     }
 
+    /**
+     * get the user id for this token
+     * @return
+     */
     @Override
     public int getUserId() {
         return userId;
     }
+
+    /**
+     * Returns the session for this token
+     * @return
+     */
     public String getSession() {
         return this.sessionId;
     }
 
+    /**
+     * What security level this token should have access to
+     * @return
+     */
     public int getSecurityLevel() {
         return securityLevel;
     }
